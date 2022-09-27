99f3ce7e9a6e3b9ca3421f29b247875c6de3a58d
Minghao PAN <MPan2@slb.com>
2022-09-27 13:57:49 +0100

    connect frontend and backend

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 import { HttpClient } from '@angular/common/http';
 import { Game } from '@bg-hoard/util-intertface';
@@ -9,10 +9,13 @@ import { Game } from '@bg-hoard/util-intertface';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    @Inject('baseUrl') private baseUrl: string
+  ) {
     console.log('component constructed');
   }
   title = 'Board Game Hoard';
-  games = this.http.get<Game[]>('/api/games');
+  games = this.http.get<Game[]>(`${this.baseUrl}/api/games`);
   formatRating = formatRating;
 }
--- apps/store/src/environments/environment.prod.ts
+++ apps/store/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://minghao-bg-hoard.herokuapp.com',
 };
